506d4818d1fc23fbc9cc62943e000576cba80b67
zionko919 <98718538+zionko919@users.noreply.github.com>
2022-08-06 23:54:50 -0500

    Add files via upload

---
+++ SHORT_PROJECTS_and_PROGRAMS/Fibonacci_Linear_Feedback_shift_Register_Simulation/FibLFSR.h
@@ -0,0 +1,22 @@
+#ifndef _FIBLFSR_H
+#define _FIBLFSR_H
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+class FibLFSR {
+public:
+    FibLFSR(string seed);
+    int step();
+    int generate(int k);
+    ~FibLFSR();
+    friend ostream& operator<< (ostream &out, const FibLFSR &fibLfsr);
+private:
+    string seed;
+};
+
+
+#endif
